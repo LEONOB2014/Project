4e8c9821c424e97fae71988e78b2434464e90d4e
LEONOB2014 <aleonardobecerra@gmail.com>
2014-08-24 20:29:17 -0500

    hello

---
+++ Plot4.R
@@ -0,0 +1,17 @@
+library("plyr")
+library("ggplot2")
+# Load data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+data<-transform(NEI,type=factor(type),year=factor(year))
+combustion<-as.data.frame(SCC[grep("combustion",SCC$SCC.Level.One,ignore.case=T) &
+                                      grep("coal",SCC$SCC.Level.Three,ignore.case=T),1])
+names(combustion)<-"SCC"
+data2<-merge(combustion,data,by="SCC")
+#Plot Data
+plotdata<-ddply(data2,.(year),summarize,sum=sum(Emissions))
+png("plot4.png")
+gplot<-ggplot(plotdata,aes(year,sum))
+gplot+geom_point(size=4)+labs(title="PM2.5 Emission from coal combustion-related sources ",
+                              y="Total PM2.5 emission each year")
+dev.off()
